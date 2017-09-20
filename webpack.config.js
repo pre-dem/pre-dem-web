3906ba835ce159cfc8a2d4fd36aa4b73d890dbfa
lansefengxinzi <774685342@qq.com>
2017-09-20 22:00:58 +0800

    delete webpack dev

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack')
 
 module.exports = {
   entry: {
-      "pre-dem-web": './src/index.ts',
+      "pre-dem-browser": './src/index.ts',
   },
   output: {
     filename: '[name].js',
