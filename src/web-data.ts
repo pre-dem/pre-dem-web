c09a2c123103595d5d16dbd62b9899121fee2916
Siyu Wang <cn.wangsiyu@gmail.com>
2017-11-20 21:20:46 +0800

    update

--- src/web-data.ts
+++ src/web-data.ts
@@ -3,7 +3,8 @@
  */
 
 import { _window } from './detection'
-import { getDominFromUrl, getCookier, setCookier, getBrowserInfo, generateUUID} from './utils'
+import { getDominFromUrl, getCookier, setCookier, generateUUID} from './utils'
+import {stringify} from "querystring";
 
 
 const packageJson = require('../package.json')
@@ -14,16 +15,12 @@ class WebData {
     appId: string;
     domain: string;
     tag: string;
-    osVersion: string;
-    osBuild: string;
     uuid: string;
 
     constructor() {
         this.appId = "";
         this.domain = "";
         this.tag = "";
-        this.osVersion = "";
-        this.osBuild = "";
 
         let predemUuid = "";
 
@@ -44,20 +41,6 @@ class WebData {
             }
             this.uuid = predemUuid;
         }
-        const BrowserInfo: any = getBrowserInfo();
-        let version = "";
-        if (BrowserInfo !== {}) {
-            version = BrowserInfo.version;
-            if (version) {
-                const versionArray = version.split(".");
-                if (versionArray.length === 1) {
-                    this.osVersion = versionArray[0];
-                } else if (versionArray.length >= 2) {
-                    this.osVersion = versionArray[0];
-                    this.osBuild = versionArray[1];
-                }
-            }
-        }
     }
 
     init(appId: string, domain: string): void {
@@ -71,7 +54,7 @@ class WebData {
 
     sendEventData(name: string, data): any {
         const url = this.postDataUrl(this.domain, "event", this.appId);
-        const eventData = this.initCustomEvent(this.appId, this.tag, name, data);
+        const eventData = this.initCustomEvent(this.tag, name, data);
         return _window._origin_fetch(url, {
             method: 'POST',
             headers: {
@@ -90,17 +73,16 @@ class WebData {
         const url = this.postDataUrl(this.domain, type, this.appId);
         let result: any;
         if (type === "performance") {
-            result = this.initPerformance(this.appId, datas, this.tag);
+            result = this.initPerformance(datas, this.tag);
         } else if (type === "error") {
-            result = this.initErrorData(this.appId, datas, this.tag);
+            result = this.initErrorData(datas, this.tag);
         } else {
             if (datas instanceof Array) {
-                const array = [];
+                result = ""
                 datas.map((data) => {
-                    const errorobj = this.initNetworkData(this.appId, data, this.tag);
-                    array.push(errorobj)
+                    result = result + JSON.stringify(this.initNetworkData(data, this.tag)) + "\n";
                 });
-                return this.getRequestFun(url, type, array.join("\n"))
+                return this.getRequestFun(url, type, result)
             }
         }
         return this.getRequestFun(url, type, result)
@@ -110,108 +92,102 @@ class WebData {
     postDataUrl(domain: string, category: string, appId: string): string {
         switch (category) {
             case 'error': {
-               return domain + '/v1/' + appId +'/crashes/' + WEB_PLATFORM;
+               return domain + '/v2/' + appId +'/crashes';
             }
             case 'performance': {
-                return domain + '/v1/' + appId +'/web/performance/' + WEB_PLATFORM;
+                return domain + '/v2/' + appId +'/web/performances';
             }
             case 'network': {
-                return domain + '/v1/' + appId +'/http-stats/' + WEB_PLATFORM;
+                return domain + '/v2/' + appId +'/http-monitors';
             }
             case 'event': {
-                return domain + '/v1/' + appId +'/events';
+                return domain + '/v2/' + appId +'/custom-events';
             }
         }
         return "";
     }
 
-    initCustomEvent(AppId: string, tag: string, name: string, content: string): any {
+    initCustomEvent(tag: string, name: string, content: string): any {
 
         return {
-            app_id: AppId,
-            app_bundle_id: window.location.host,
-            sdk_version: VERSION,
-            sdk_id: this.uuid,
-            tag: tag,
-            type: "custom",
-            name: name,
-            content: content,
+            time:           Date.now(),
+            type:           "custom",
+            name:           name,
+            sdk_version:    VERSION,
+            sdk_id:         this.uuid,
+            tag:            tag,
+            domain:         window.location.host,
+            path:           window.location.pathname,
+            content:        content,
         }
 
     }
 
-    initPerformance(AppId: string, message: any, tag: string): any {
+    initPerformance(message: any, tag: string): any {
         const resourceTiming = message.payload.resourceTiming;
-        const performance = {
-            app_id:         AppId,
+        return {
+            time:           Date.now(),
+            type:           "auto_captured",
+            name:           "performance",
+            sdk_version:    VERSION,
+            sdk_id:         this.uuid,
             tag:            tag,
             domain:         window.location.host,
             path:           window.location.pathname,
+            content:        JSON.stringify({
+                resourceTiming: JSON.stringify(resourceTiming)
+            })
         };
-
-        return  Object.assign(
-            performance,
-            {resourceTiming: JSON.stringify(resourceTiming)},
-        )
     };
 
-    initNetworkData(AppId: string, message: any, tag: string): any {
+    initNetworkData(message: any, tag: string): any {
         const networkErrorCode = message.payload.status_code !== 200 ? message.payload.status_code : 0;
         const networkErrorMsg = message.payload.status_code !== 200 ? message.payload.responseText : "";
         const dataLength = message.payload.contentLength ? message.payload.contentLength : 0;
         const responseTimeStamp =  message.payload.ResponseTimeStamp ? message.payload.ResponseTimeStamp : 0;
-        const network  = {
-            AppBundleId:        window.location.host,
-            AppName:            "",
-            AppVersion:         "",
-            DeviceModel:        "",
-            OsPlatform:         "",
-            OsVersion:          "",
-            OsBuild:            "",
-            SdkVersion:         VERSION,
-            SdkId:              this.uuid,
-            DeviceId:           "",
-            Tag:                tag,
-            Manufacturer:       "",
-            Domain:             getDominFromUrl(message.payload.url).domain,
-            Path:               getDominFromUrl(message.payload.url).path,
-            Method:             message.payload.method,
-            HostIP:             "",
-            StatusCode:         message.payload.status_code,
-            StartTimestamp:     message.timestamp,
-            ResponseTimeStamp:  responseTimeStamp,
-            EndTimestamp:       message.timestamp +  message.payload.duration,
-            DnsTime:            0,
-            DataLength:         dataLength,
-            NetworkErrorCode:   networkErrorCode,
-            NetworkErrorMsg:    networkErrorMsg,
-        };
-
-        const networkData = [];
-        for (const key in network) {
-            if (network[key] === 0) {
-                networkData.push(0)
-            } else if (!network[key] || network[key] === "" || network[key] === null) {
-                networkData.push("-")
-            } else {
-                networkData.push(network[key])
-            }
-        }
-        return networkData.join("\t");
-    }
-
-    initErrorData(AppId: string, message: any, tag: string): any {
-        const crash_log_key = JSON.stringify(message.payload.stack);
         return {
-            app_id:         AppId,
-            app_bundle_id:  window.location.host,
+            time:           Date.now(),
+            type:           "auto_captured",
+            name:           "monitor",
             sdk_version:    VERSION,
             sdk_id:         this.uuid,
             tag:            tag,
-            crash_log_key:  crash_log_key,
-            crash_time:     message.timestamp,
-            mode:           message.payload.mode,
-            message:        message.payload.message,
+            domain:         window.location.host,
+            path:           window.location.pathname,
+            content:        JSON.stringify({
+                domain: getDominFromUrl(message.payload.url).domain,
+                path: getDominFromUrl(message.payload.url).path,
+                method: message.payload.method,
+                host_ip: "",
+                status_code: message.payload.status_code,
+                start_timestamp: message.timestamp,
+                response_time_stamp: responseTimeStamp,
+                end_timestamp: message.timestamp + message.payload.duration,
+                dns_time: 0,
+                data_length: dataLength,
+                network_error_code: networkErrorCode,
+                network_error_msg: networkErrorMsg,
+            })
+        };
+    }
+
+    initErrorData(message: any, tag: string): any {
+        const crash_log_key = JSON.stringify(message.payload.stack);
+        return {
+            time:           Date.now(),
+            type:           "auto_captured",
+            name:           "monitor",
+            sdk_version:    VERSION,
+            sdk_id:         this.uuid,
+            tag:            tag,
+            domain:         window.location.host,
+            path:           window.location.pathname,
+            content:        JSON.stringify({
+                crash_log_key: crash_log_key,
+                crash_time: message.timestamp,
+                mode: message.payload.mode,
+                message: message.payload.message,
+            })
         }
     }
 
@@ -241,7 +217,7 @@ class WebData {
         return _window._origin_fetch(url, {
             method: 'POST',
             headers: {
-                'Content-Type': 'text/plain',
+                'Content-Type': 'application/json',
             },
             body: result,
         })
