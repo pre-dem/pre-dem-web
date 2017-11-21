2f70b84267dfeda8ad388060a29e5d742b175f6b
lansefengxinzi <774685342@qq.com>
2017-11-21 17:03:16 +0800

    format code

--- src/sources/performance.ts
+++ src/sources/performance.ts
@@ -2,52 +2,52 @@ import Source from '../source'
 
 export default () => {
 
-    return new Source('performance', (action) => {
-        window.onload = function() {
+  return new Source('performance', (action) => {
+    window.onload = function () {
 
-            let timing = null;
-            let resourceTiming = [];
+      let timing = null;
+      let resourceTiming = [];
 
-            if (!window.performance) {
-                return false;
+      if (!window.performance) {
+        return false;
+      }
+
+      timing = performance.timing;
+
+
+      if (window.performance.getEntries) {
+        const timings = window.performance.getEntries();
+        if (timings && timings.length > 0) {
+          const newResourceTiming = [];
+          timings.map((timing: any) => {
+            if (timing.entryType === "resource"
+              && timing.encodedBodySize
+              && timing.duration
+              && timing.encodedBodySize > 0
+              && timing.duration > 0) {
+              newResourceTiming.push(timing);
             }
 
-            timing = performance.timing;
+          });
+
+          action({
+            category: 'performance',
+            payload: {timing, resourceTiming: newResourceTiming}
+          });
+        }
+
+      } else {
+        action({
+          category: 'performance',
+          payload: {timing, resourceTiming: []}
+        });
+      }
 
 
-            if (window.performance.getEntries) {
-                const timings =  window.performance.getEntries();
-                if (timings && timings.length > 0) {
-                    const newResourceTiming = [];
-                    timings.map((timing: any) => {
-                        if (timing.entryType === "resource"
-                            && timing.encodedBodySize
-                            && timing.duration
-                            && timing.encodedBodySize > 0
-                            && timing.duration > 0) {
-                            newResourceTiming.push(timing);
-                        }
-
-                    });
-
-                    action({
-                        category: 'performance',
-                        payload:  {timing, resourceTiming: newResourceTiming}
-                    });
-                }
-
-            } else {
-                action({
-                    category: 'performance',
-                    payload:  {timing, resourceTiming: []}
-                });
-            }
+    };
 
 
-        };
-
-
-    })
+  })
 
 
 }
