bcab27946277dc30e0c1f5eb6a1c05a67feaf380
lansefengxinzi <774685342@qq.com>
2018-03-13 11:15:51 +0800

    compatible jquery and verison -> v1.0.15 (#40)
    * compatible jquery and verison -> v1.0.15
    
    * 不兼容jquery 1.6以下的版本
    
    * 不兼容jquery 1.6以下的版本
    
    * v1.0.15 -> v1.0.16
    
    * update read me
    
    * update read me

--- README.md
+++ README.md
@@ -4,6 +4,31 @@ npm install
 npm run build
 
 # 使用
-dist 文件夹中 pre-dem-web.js 文件引入到 HTML 页面中
+dist 文件夹中 pre-dem-web-xx.js 文件引入到 HTML 页面中
 
-<script src="pre-dem-web.js" type="text/javascript"/>
+- 引入 js 文件
+```
+ <script type="text/javascript"
+         src="./../dist/pre-dem-web-v1.0.16.js"
+         data-app-key="${AppKey}"
+         data-domain="${Domain}"
+         data-ajax-enabled="${true | false}"
+         data-crash-enabled="${true | false}"
+         data-performance-enable="${true | false}"
+         send-buffer-capacity="${Int}"></script>   
+```     
+- 参数说明
+| 参数 | 类型 | 说明 |
+| - | - |
+| src | string | sdk 路径 |
+| data-app-key | string | AppKey |
+| data-domain | string | Domain |
+| data-ajax-enabled | string | 是否开启 Ajax 上报,  "ture" or "false"|
+| data-crash-enabled | string | 是否开启错误上报,  "ture" or "false"|
+| data-performance-enable | string | 是否开启性能数据上报,  "ture" or "false"|
+| send-buffer-capacity | string | 设置缓冲区的大小,ajax 合并发送的条数,  for example: "1"|
+
+        
+# 注意
+不兼容 jquery 1.6 以下的版本
+
--- src/sources/xhr.ts
+++ src/sources/xhr.ts
@@ -12,9 +12,10 @@ export interface IXHRMessage extends ISourceMessage {
     url: string
     status_code?: string
     duration?: number // 0 for timeout
-    responseText?: string
-    responseTimestamp?: number
-    contentLength?: number
+    response_text?: string
+    start_timestamp?: number
+    response_timestamp?: number
+    content_length?: number
   }
 }
 
@@ -50,7 +51,7 @@ export default (dem: Dem) => {
       fill(xhrproto, 'send', (originFunc) => {
         return function(data) { // preserve arity
           const xhr = this
-          const startAt = Date.now()
+          xhr.__dem_xhr.start_timestamp = Date.now();
           const timeChecker = setTimeout(() => action({
             category: 'network',
             payload: xhr.__dem_xhr
@@ -58,7 +59,7 @@ export default (dem: Dem) => {
 
           function onreadystatechangeHandler() {
             if (xhr.__dem_xhr && (xhr.readyState === 2)) {
-                xhr.__dem_xhr.responseTimestamp = Date.now()
+                xhr.__dem_xhr.response_timestamp = Date.now()
             }
             if (xhr.__dem_xhr && (xhr.readyState === 1 || xhr.readyState === 4)) {
                 if (timeChecker) {
@@ -68,11 +69,12 @@ export default (dem: Dem) => {
               try {
                 // touching statusCode in some platforms throws
                 // an exception
+                xhr.__dem_xhr.end_imestamp = Date.now()
                 xhr.__dem_xhr.status_code = xhr.status
-                xhr.__dem_xhr.duration = Date.now() - startAt
-                xhr.__dem_xhr.responseText = xhr.responseText
-                const contentLength = xhr.responseText ? xhr.responseText.length : 0;
-                xhr.__dem_xhr.contentLength = contentLength;
+                xhr.__dem_xhr.duration = xhr.__dem_xhr.end_timestamp - xhr.__dem_xhr.start_timestamp
+                xhr.__dem_xhr.response_text = xhr.responseText
+                const contentLength = xhr.responseText ? xhr.responseText.length : 0
+                xhr.__dem_xhr.content_length = contentLength
               } catch (e) { /* do nothing */ }
               action({
                 category: 'network',
@@ -81,15 +83,58 @@ export default (dem: Dem) => {
             }
           }
 
+          function onloadstart(e) {
+             xhr.__dem_xhr.response_timestamp = Date.now()
+          }
+
+          function onload(e) {
+            xhr.__dem_xhr.status_code = e.target.status
+            xhr.__dem_xhr.response_text = e.target.responseText
+            xhr.__dem_xhr.content_length = e.total
+            xhr.__dem_xhr.end_timestamp = Date.now()
+            xhr.__dem_xhr.duration = xhr.__dem_xhr.end_timestamp - xhr.__dem_xhr.start_timestamp
+            action({
+              category: 'network',
+              payload: xhr.__dem_xhr
+            })
+          }
+
           const props = [ 'onload', 'onerror', 'onprogress' ]
           for (const prop of props) {
             wrapProp(prop, xhr)
           }
 
-          if ('onreadystatechange' in xhr && isFunction(xhr.onreadystatechange)) {
-            fill(xhr, 'onreadystatechange', (orig) => dem.wrap(orig, undefined, onreadystatechangeHandler))
+          const jqueryVersion = window["$"] ? window["$"].prototype.jquery : ""
+          if (jqueryVersion !== "") {
+            const array = jqueryVersion.split(".")
+            const firstVersion = array[0]
+            const secondVersion = array[1]
+              if (parseInt(firstVersion) < 2 && parseInt(secondVersion) >= 6) { // 低版本 使用 onload, 不兼容 1.5 以下包含1.5
+
+                if ('onloadstart' in xhr && isFunction(xhr.onloadstart)) {
+                    fill(xhr, 'onloadstart', (orig) => dem.wrap(orig, undefined, onloadstart))
+                } else {
+                    xhr.onloadstart = onloadstart
+                }
+
+                if ('onload' in xhr && isFunction(xhr.onload)) {
+                    fill(xhr, 'onload', (orig) => dem.wrap(orig, undefined, onload))
+                } else {
+                    xhr.onload = onload
+                }
+
+            } else if (parseInt(firstVersion) >= 2){ // 高版本 使用 onreadystatechange
+                if ('onreadystatechange' in xhr && isFunction(xhr.onreadystatechange)) {
+                    fill(xhr, 'onreadystatechange', (orig) => dem.wrap(orig, undefined, onreadystatechangeHandler))
+                } else {
+                    xhr.onreadystatechange = onreadystatechangeHandler
+                }
+            } else {
+                  console.error("jquert 版本过低,不兼容")
+              }
+
           } else {
-            xhr.onreadystatechange = onreadystatechangeHandler
+            console.error("jquert not exist")
           }
 
           return originFunc.apply(this, arguments)
