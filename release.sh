9f3d592e7ea7db6230868b1bdf1ddc8380e00866
coolking <codingyjt@gmail.com>
2019-10-08 00:19:37 +0800

    增加 npm 引入的方式 (#50)
    * add npm package entry
    
    * adjust detail
    
    * fix dependencies

---
+++ release.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+
+tagName="v$(jq '.version' package.json | sed "s/\"//g")"
+
+# check if tag aleady exists
+if [[ -n $(git ls-remote --tags origin $tagName) ]]; then
+  echo "tag $tagName already exists, skip release"
+  exit 0
+fi
+
+# install deps & do compile
+yarn
+mv ./src/main.ts ./src/index.ts
+npm run compile
+
+# move compile result out
+rm -rf ./src
+mv ./dist ./lib
+mv -f .gitignore_for_release .gitignore
+
+# remove dist folder
+rm -rf ./dist
+
+# commit & push
+git add --all .
+git commit -m "build"
+git tag -a $tagName -m "built by script"
+git push origin $tagName
---
+++ src/main.ts
@@ -0,0 +1,111 @@
+import dem from "./dem"
+import Transfer from "./transfer"
+import webData from './web-data'
+import Transaction from "./transaction"
+
+const APP_KEY_LENGTH = 24
+const APP_ID_LENGTH = 8
+
+export interface IPredemOptions {
+    appKey: string
+    domain: string
+    httpEnabled?: boolean // 是否开启 http 请求上报
+    crashEnabled?: boolean // 是否开启错误上报
+    performanceEnabled?: boolean // 是否开启性能上报
+    bufferCapacity?: number // 缓冲区的大小, 合并发送的条数
+}
+
+
+export class Predem {
+    init(options: IPredemOptions) {
+        const { appKey, domain, httpEnabled, crashEnabled, performanceEnabled, bufferCapacity } = options
+
+        if (!appKey || appKey.length !== APP_KEY_LENGTH) {
+            console.error("appKey is invalid")
+            return
+        }
+
+        if (!domain) {
+            console.error("domain is not defined")
+            return
+        }
+
+        dem.messages.apiDomain = domain
+        if (Number.isInteger(bufferCapacity)) {
+            dem.messages.messageThreshold = bufferCapacity
+        }
+
+        const appId = appKey.substring(0, APP_ID_LENGTH)
+        webData.init(appId, domain, `${!!httpEnabled}`, `${!!crashEnabled}`, `${!!performanceEnabled}`)
+
+        this.initTransfer()
+    }
+
+    setTag(tag: string): void {
+        webData.setTag(tag)
+    }
+
+    setAppVersion(version: string): void {
+        webData.setVersion(version)
+    }
+
+    setPerformanceFilter(filterFunc): void {
+        if (!filterFunc) {
+            console.error("filter 不能为空！")
+            return
+        }
+
+        if (!(filterFunc instanceof Function)) {
+            console.error("filter 必须是 Function！")
+            return
+        }
+        webData.setPerformanceFilter(filterFunc)
+    }
+
+    captureException(err: Error): void {
+        dem.captureException(err)
+    }
+
+    sendEvents(events: any[]): any {
+        if (!(events instanceof Array)) {
+            console.log("Custom data need type Array")
+            return
+        }
+
+        if (events.length === 0) {
+            console.error("Custom data can not be empty")
+            return
+        }
+        const event = events[0]
+        if (event.eventName === "undefine" || event.eventData === "undefine") {
+            console.error("Custom data must have eventName and eventData")
+            return
+        }
+
+        return webData.sendEventData(events)
+    }
+
+    sendEvent(event) {
+        if (!event) {
+            return
+        }
+        this.sendEvents([event])
+    }
+
+    transactionStart(name: string) {
+        return new Transaction(name)
+    }
+
+    private initTransfer() {
+        const testTransfer = new Transfer(webData.tag, (datas, callback) => {
+            webData.push(datas)
+            callback()
+        })
+        dem.addTransfer(testTransfer)
+    }
+}
+
+
+const predem = new Predem()
+
+export default predem
